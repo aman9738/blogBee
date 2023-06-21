55997919c5df450796c7c526c4a06b43ab16ed36
Aman Agarwal <amanagarwal9738@gmail.com>
2023-06-21 21:02:46 +0530

    fix typo

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -30,7 +30,7 @@ const Blog = async () => {
   return (
     <div className={styles.mainContainer}>
       {data.map((item) => (
-        <Link href={`/blog/${item._id}`} className={styles.container} key={item.id}>
+        <Link href={`/blog/${item._id}`} className={styles.container} key={item._id}>
           <div className={styles.imageContainer}>
             <Image
               src={item.img}
